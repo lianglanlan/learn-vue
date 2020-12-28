ba55d29c296838b478c934a65c2e6ba8a03e8b0a
lianglanlan <799679063@qq.com>
2020-12-28 23:19:44 +0800

    43集改变购买数量提交

---
+++ 案例2书籍购物车/main.js
@@ -0,0 +1,45 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        books: [
+            {
+                id: 1,
+                name: '《算法导论》',
+                date: '2006-9',
+                price: 85,
+                count: 1
+            }, {
+                id: 2,
+                name: '《算法导论》',
+                date: '2006-9',
+                price: 59,
+                count: 1
+            }, {
+                id: 3,
+                name: '《算法导论》',
+                date: '2006-9',
+                price: 39,
+                count: 1
+            }, {
+                id: 4,
+                name: '《算法导论》',
+                date: '2006-9',
+                price: 128,
+                count: 1,
+            }
+        ]
+    },
+    filters: {
+        showPrice(price) {
+            return '￥' + parseInt(price).toFixed(2)
+        }
+    },
+    methods: {
+        increment(index) {
+            this.books[index].count++
+        },
+        decrement(index) {
+            this.books[index].count--
+        }
+    },
+})
