1283b4fb7d2bbe668fd91f33524293a1d2ef12bc
lianglanlan <799679063@qq.com>
2021-01-07 22:35:34 +0800

    78集webpack配置提交

--- 4.webpack/起步/webpack.config.js
+++ 4.webpack/起步/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         // path: './dist', //这样写会报错，需要写绝对路径
-        path: '',
+        path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
     }
 }
