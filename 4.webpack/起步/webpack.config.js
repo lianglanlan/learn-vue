d2a8d67a39ed5d16f07ac52c1380db1226bd8ba7
lianglanlan <799679063@qq.com>
2021-01-07 19:48:01 +0800

    提交

---
+++ 4.webpack/起步/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        // path: './dist', //这样写会报错，需要写绝对路径
+        path: '',
+        filename: 'bundle.js'
+    }
+}
