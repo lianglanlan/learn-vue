8bdc046d58d5e40b00cff9fecd4b8c77864022d7
lianglanlan <799679063@qq.com>
2021-01-09 17:44:56 +0800

    79添加css loader配置

--- 4.webpack/起步/src/main.js
+++ 4.webpack/起步/src/main.js
@@ -4,3 +4,6 @@ console.log(add(20, 30))
 
 import { name, age, height } from './js/info'
 console.log(name)
+
+//添加这行代码后打包报错
+require('./css/normal.css')
--- 4.webpack/起步/webpack.config.js
+++ 4.webpack/起步/webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
         // path: './dist', //这样写会报错，需要写绝对路径
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                //css-loader负责将css文件进行加载
+                //style-loader负责将样式添加到DOM中
+                //webpack使用多个loader时是从右向左执行，所以应该把css-loader放在右边
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 }
