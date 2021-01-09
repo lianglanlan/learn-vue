bede50a9031ffb760aaf44713a82c28585434372
lianglanlan <799679063@qq.com>
2021-01-09 17:26:04 +0800

    新建js目录,将非入口文件 移入js目录中

--- 4.webpack/起步/src/main.js
+++ 4.webpack/起步/src/main.js
@@ -1,6 +1,6 @@
-const { add, mul } = require('./mathUtils.js')
+const { add, mul } = require('./js/mathUtils')
 
 console.log(add(20, 30))
 
-import { name, age, height } from './info'
+import { name, age, height } from './js/info'
 console.log(name)
--- 4.webpack/起步/webpack.config.js
+++ 4.webpack/起步/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         // path: './dist', //这样写会报错，需要写绝对路径
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     }
 }
