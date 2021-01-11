628c8ce197d7f8a3d0fa7042957ef316897ed743
lianglanlan <799679063@qq.com>
2021-01-11 19:39:03 +0800

    提交

---
+++ 4.webpack/+vue/src/vue/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <button @click="btnClick">按钮</button>
+    <h2>{{ name }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "hello webpack",
+      name: "小明",
+    };
+  },
+  methods: {
+    btnClick() {
+      console.log(1);
+    },
+  },
+};
+</script>
+<style>
+</style>
