fba1cb1035c7bbd21014fee8eff43aa38a1dcea4
lianglanlan <799679063@qq.com>
2021-01-22 14:35:31 +0800

    103集提交，vue-router的安装和配置

--- 7.vuecli2test2/src/main.js
+++ 7.vuecli2test2/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router' //后面的index可跟可不跟，会自动找目录中的index文件
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ new Vue({
   // render: h => h('h2',
   //   { class: 'box' },
   //   ['hello world', h('button', ['按钮'])]) //还可以这样使用
-  render: h => h(App) //App作为对象传入
+  render: h => h(App), //App作为对象传入
+  router
 })
