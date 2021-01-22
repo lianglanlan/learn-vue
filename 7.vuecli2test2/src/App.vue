1667cc8bb40e31eb7399c1da6377167ca688d1b2
lianglanlan <799679063@qq.com>
2021-01-22 16:03:40 +0800

    106提交active-class属性

--- 7.vuecli2test2/src/App.vue
+++ 7.vuecli2test2/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <!-- replace不会留下history记录 -->
-    <router-link to="/home" tag="button" replace>首页</router-link>
-    <router-link to="/about" tag="button">关于</router-link>
+    <router-link to="/home" tag="button" replace active-class="active"
+      >首页</router-link
+    >
+    <!-- active-class默认值router-link-active -->
+    <router-link to="/about" tag="button" active-class="active"
+      >关于</router-link
+    >
     <router-view></router-view>
   </div>
 </template>
@@ -22,5 +27,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.active {
+  color: red;
 }
 </style>
