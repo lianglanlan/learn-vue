4dbd83b7fbcfa5aa051358625d675ab85ca3520d
lianglanlan <799679063@qq.com>
2021-01-26 10:27:31 +0800

    120提交将tabbar提取为组件

--- 10.tabbar/src/App.vue
+++ 10.tabbar/src/App.vue
@@ -1,36 +1,19 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <tab-bar></tab-bar>
   </div>
 </template>
 
 <script>
+import TabBar from "./components/tabbar/TabBar";
 export default {
   name: "App",
+  components: {
+    TabBar,
+  },
 };
 </script>
 
 <style>
 @import "./assets/css/base.css";
-#tab-bar {
-  position: fixed;
-  left: 0;
-  width: 100%;
-  bottom: 0;
-  height: 49px;
-  display: flex;
-  background-color: #f1f1f1;
-  box-shadow: rgba(0, 0, 0, 0.2) 0 0 5px 0;
-}
-
-.tab-bar-item {
-  flex: 1;
-  text-align: center;
-  line-height: 49px;
-}
 </style>
---
+++ 10.tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="tab-bar">
+    <div class="tab-bar-item">首页</div>
+    <div class="tab-bar-item">分类</div>
+    <div class="tab-bar-item">购物车</div>
+    <div class="tab-bar-item">我的</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
+<style scoped>
+#tab-bar {
+  position: fixed;
+  left: 0;
+  width: 100%;
+  bottom: 0;
+  height: 49px;
+  display: flex;
+  background-color: #f1f1f1;
+  box-shadow: rgba(0, 0, 0, 0.2) 0 0 5px 0;
+}
+
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  line-height: 49px;
+}
+</style>
