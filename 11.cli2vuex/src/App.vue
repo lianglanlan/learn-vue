ee74463ba0480843d896822300a2a87944281c78
lianglanlan <799679063@qq.com>
2021-01-27 17:29:12 +0800

    脚手架搭建目录

---
+++ 11.cli2vuex/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      message: "我是App组件",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
