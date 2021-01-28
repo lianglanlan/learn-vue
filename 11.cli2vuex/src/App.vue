6c251392b61e5beea0e68704fec2b9d27e3aa29e
lianglanlan <799679063@qq.com>
2021-01-28 11:17:16 +0800

    131提交使用父子组件传值

--- 11.cli2vuex/src/App.vue
+++ 11.cli2vuex/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
     <h2>{{ message }}</h2>
+    <h2>{{ counter }}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+    <hello-vuex :counter="counter" />
   </div>
 </template>
 
 <script>
+import HelloVuex from "./components/HelloVuex";
 export default {
   name: "App",
-  components: {},
+  components: {
+    HelloVuex,
+  },
   data() {
     return {
       message: "我是App组件",
+      counter: 0,
     };
   },
 };
