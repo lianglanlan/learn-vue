43d5af0c78333a3e70cc11adfeb4b86f0ce3c295
lianglanlan <799679063@qq.com>
2021-02-01 19:23:07 +0800

    143提交axios基本使用

--- 11.cli2vuex/src/main.js
+++ 11.cli2vuex/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 
+import axios from 'axios'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -10,3 +12,13 @@ new Vue({
   render: h => h(App),
   store
 })
+
+axios({
+  url: 'http://123.207.32.32:8000/home/multidata',
+  method: 'get', //默认为get请求
+  //参数
+  params: {
+    type: 'pop',
+    page: 1
+  }
+}).then(res => console.log(res))
