a751ad9278d61a7dae94ad5f5583618e8684b456
lianglanlan <799679063@qq.com>
2021-02-02 16:36:11 +0800

    147提交axios拦截器

--- 11.cli2vuex/src/network/request.js
+++ 11.cli2vuex/src/network/request.js
@@ -5,6 +5,22 @@ export function request(config) {
         baseURL: 'http://123.207.32.32:8000',
         timeout: 5000
     })
+    //请求拦截器
+    instance.interceptors.request.use(config => {
+        //1.比如config中的一些信息不符合服务器要求
+        //2.每次发送网络请求时，希望在页面中显示一个loading图标
+        //3.某些网络请求，比如登录，必须携带一些加密信息
+        return config
+    }, err => {
+        return Promise.reject(err);
+    })
+    //响应拦截器
+    instance.interceptors.response.use(res => {
+        return response;
+    }, err => {
+        return Promise.reject(err);
+    })
+
     //发送真正的网络请求
     return instance(config)
 }
