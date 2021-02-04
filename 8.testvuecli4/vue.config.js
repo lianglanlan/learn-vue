87da3e9b3603974170c990865887a3396f5819e9
lianglanlan <799679063@qq.com>
2021-02-04 10:47:48 +0800

    别名配置

--- 8.testvuecli4/vue.config.js
+++ 8.testvuecli4/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-
+    configureWebpack: {
+        resolve: {
+            alias: {
+                //@符号已被配置为src
+                'assets': '@/assets',
+                'common': '@/common',
+                'network': '@/network',
+                'components': '@/components',
+            }
+        }
+    }
 }
