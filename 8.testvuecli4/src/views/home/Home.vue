4039844bdeb8b38b31f5b4f384503451d190cb91
lianglanlan <799679063@qq.com>
2021-02-04 15:16:58 +0800

    154提交首页导航栏

--- 8.testvuecli4/src/views/home/Home.vue
+++ 8.testvuecli4/src/views/home/Home.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
+<script>
+import NavBar from "../../components/common/navbar/NavBar";
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+};
+</script>
+<style scoped>
+.nav-bar {
+  background-color: var(--color-tint);
+  font-weight: 700;
+  color: #fff;
+}
+</style>
