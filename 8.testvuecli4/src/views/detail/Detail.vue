a71e068abdc62bb70728f575e7a92cfff86a899e
lianglanlan <799679063@qq.com>
2021-03-02 21:39:17 +0800

    182提交，拿到轮播图数据

--- 8.testvuecli4/src/views/detail/Detail.vue
+++ 8.testvuecli4/src/views/detail/Detail.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
   </div>
 </template>
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
 
 import { getDetail } from "../../network/detail";
 
 export default {
   components: {
     DetailNavBar,
+    DetailSwiper,
   },
   data() {
     return {
       iid: null,
+      topImages: [],
     };
   },
   created() {
@@ -23,6 +27,8 @@ export default {
     //根据iid请求详情数据
     getDetail(this.iid).then((res) => {
       console.log(res);
+      //对res做数据分离
+      this.topImages = res.result.itemInfo.topImages;
     });
   },
 };
---
+++ 8.testvuecli4/src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{topImages}}</div>
+</template>
+<script>
+export default {
+  props: {
+    topImages: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
