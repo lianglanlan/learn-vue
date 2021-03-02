3a97e8c57ca782304d97c9ba6ef05ef7308dae88
lianglanlan <799679063@qq.com>
2021-03-02 22:58:02 +0800

    184提交添加数据对象

--- 8.testvuecli4/src/views/detail/Detail.vue
+++ 8.testvuecli4/src/views/detail/Detail.vue
@@ -8,7 +8,7 @@
 import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
 
-import { getDetail } from "../../network/detail";
+import { getDetail, Goods, Shop } from "../../network/detail";
 
 export default {
   name: "Detail",
@@ -20,6 +20,8 @@ export default {
     return {
       iid: null,
       topImages: [],
+      goods: {},
+      shop: {},
     };
   },
   created() {
@@ -28,8 +30,18 @@ export default {
     //根据iid请求详情数据
     getDetail(this.iid).then((res) => {
       console.log(res);
+      const data = res.result;
       //对res做数据分离
-      this.topImages = res.result.itemInfo.topImages;
+      this.topImages = data.itemInfo.topImages;
+
+      //获取商品信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+      //获取店铺信息
+      this.shop = new Shop(data.shopInfo);
     });
   },
 };
