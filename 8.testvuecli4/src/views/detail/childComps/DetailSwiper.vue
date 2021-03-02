b374911e7717df78893283e706fce0aad554e9df
lianglanlan <799679063@qq.com>
2021-03-02 21:45:33 +0800

    182提交轮播图数据绑定

--- 8.testvuecli4/src/views/detail/childComps/DetailSwiper.vue
+++ 8.testvuecli4/src/views/detail/childComps/DetailSwiper.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>{{topImages}}</div>
+  <div class="detail-swiper">
+    <swiper>
+      <swiper-item v-for="(item,index) in topImages" :key="index" class="swiper-item">
+        <img :src="item" alt />
+      </swiper-item>
+    </swiper>
+  </div>
 </template>
 <script>
+import { Swiper, SwiperItem } from "../../../components/common/swiper";
 export default {
+  components: {
+    Swiper,
+    SwiperItem,
+  },
   props: {
     topImages: {
       type: Array,
@@ -13,3 +24,9 @@ export default {
   },
 };
 </script>
+<style scoped>
+.swiper-item {
+  height: 300px;
+  overflow: hidden;
+}
+</style>
