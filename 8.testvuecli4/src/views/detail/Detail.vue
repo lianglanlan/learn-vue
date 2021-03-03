f56272b5092ddc2a1e2b1761e5ba81d119dd920d
lianglanlan <799679063@qq.com>
2021-03-03 16:35:32 +0800

    183提交店铺信息组件

--- 8.testvuecli4/src/views/detail/Detail.vue
+++ 8.testvuecli4/src/views/detail/Detail.vue
@@ -3,12 +3,14 @@
     <detail-nav-bar></detail-nav-bar>
     <detail-swiper :top-images="topImages"></detail-swiper>
     <detail-base-info :goods="goods"></detail-base-info>
+    <detail-shop-info :shop="shop"></detail-shop-info>
   </div>
 </template>
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
 import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import DetailShopInfo from "./childComps/DetailShopInfo";
 
 import { getDetail, Goods, Shop } from "../../network/detail";
 
@@ -18,6 +20,7 @@ export default {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
+    DetailShopInfo,
   },
   data() {
     return {
