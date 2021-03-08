d3b87a9a09f5d791a84c83bc03e0570154fd12db
lianglanlan <799679063@qq.com>
2021-03-08 22:05:27 +0800

    199提交参数部分

--- 8.testvuecli4/src/views/detail/childComps/DetailParamInfo.vue
+++ 8.testvuecli4/src/views/detail/childComps/DetailParamInfo.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>{{paramInfo}}</div>
+  <div class="param-info" v-if="Object.keys(paramInfo).length!==0">
+    <table class="info-size" v-for="(table,index) in paramInfo.sizes" :key="index">
+      <tr v-for="(tr,indey) in table" :key="indey">
+        <td v-for="(td,indez) in tr" :key="indez">{{td}}</td>
+      </tr>
+    </table>
+    <table class="info-param">
+      <tr v-for="(info,index) in paramInfo.infos" :key="index">
+        <td class="info-param-key">{{info.key}}</td>
+        <td class="param-value">{{info.value}}</td>
+      </tr>
+    </table>
+    <div class="info-img" v-if="paramInfo.image">
+      <img :src="paramInfo.image" alt />
+    </div>
+  </div>
 </template>
 <script>
 export default {
