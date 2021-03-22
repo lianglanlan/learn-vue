2ecb23d861f6279b0da34fa4e2bcc61dedff8dc4
lianglanlan <799679063@qq.com>
2021-03-22 10:46:28 +0800

    214提交vuex添加getters

--- 8.testvuecli4/src/views/cart/Cart.vue
+++ 8.testvuecli4/src/views/cart/Cart.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>购物车</div>
+  <div>
+    <nav-bar class="nav-bar"
+      ><div slot="center">购物车({{ length }})</div></nav-bar
+    >
+  </div>
 </template>
+<script>
+import NavBar from "../../components/common/navbar/NavBar";
+
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Cart",
+  data() {
+    return {};
+  },
+  components: {
+    NavBar,
+  },
+  computed: {
+    ...mapGetters({
+      length: "cartLength",
+      list: "cartList",
+    }),
+  },
+};
+</script>
+<style>
+.nav-bar {
+  background-color: var(--color-tint);
+  font-weight: 700;
+  color: #fff;
+}
+</style>
