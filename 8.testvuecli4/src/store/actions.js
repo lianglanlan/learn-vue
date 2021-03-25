db4e84560f33d0543d5b1cd552311a5822ef7ee1
lianglanlan <799679063@qq.com>
2021-03-25 10:53:11 +0800

    222提交actions返回promise

--- 8.testvuecli4/src/store/actions.js
+++ 8.testvuecli4/src/store/actions.js
@@ -2,11 +2,15 @@ import { ADD_COUNTER, ADD_TO_CART } from './mutation-types'
 
 export default {
     addCart({ commit, state }, payload) {
-        let oldProduct = state.cartList.find(item => item.iid === payload.iid)
-        if (oldProduct) {
-            commit(ADD_COUNTER, oldProduct)
-        } else {
-            commit(ADD_TO_CART, payload)
-        }
+        return new Promise((resolve, reject) => {
+            let oldProduct = state.cartList.find(item => item.iid === payload.iid)
+            if (oldProduct) {
+                commit(ADD_COUNTER, oldProduct)
+                resolve('当前的商品数量+1')
+            } else {
+                commit(ADD_TO_CART, payload)
+                resolve('添加新商品')
+            }
+        })
     }
 }
--- 8.testvuecli4/src/views/detail/Detail.vue
+++ 8.testvuecli4/src/views/detail/Detail.vue
@@ -157,7 +157,9 @@ export default {
       product.price = this.goods.realPrice;
       product.iid = this.iid;
       //将商品添加到购物车
-      this.$store.dispatch("addCart", product);
+      this.$store.dispatch("addCart", product).then(res=>{
+        console.log(res)
+      })
     },
   },
   destroyed() {
