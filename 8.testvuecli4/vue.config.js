9a75abd677134a3dfeadd7ede7154c3355f5eed7
lianglanlan <799679063@qq.com>
2021-04-06 11:43:22 +0800

    添加图片压缩

--- 8.testvuecli4/vue.config.js
+++ 8.testvuecli4/vue.config.js
@@ -29,6 +29,17 @@ module.exports = {
             .use('url-loader')
             .loader('url-loader')
             .tap(options => Object.assign(options, { limit: 1000 }))
+        config.module
+            .rule("images")
+            .use("image-webpack-loader")
+            .loader("image-webpack-loader")
+            .options({
+                mozjpeg: { progressive: true, quality: 65 },
+                optipng: { enabled: false },
+                pngquant: { quality: [0.75, 0.90], speed: 4 },
+                gifsicle: { interlaced: false },
+                webp: { quality: 75 }
+            });
     },
     css: {
         loaderOptions: {
